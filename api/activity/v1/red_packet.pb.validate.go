5de05ec08cfb6913991a9b6c62c02627f6a6a9bb
lpw <liupengwei9199@dingtalk.com>
2024-01-29 14:02:50 +0800

    red-packet

--- api/activity/v1/red_packet.pb.validate.go
+++ api/activity/v1/red_packet.pb.validate.go
@@ -703,6 +703,40 @@ func (m *GetShareConfReply) validate(all bool) error {
 
 	// no validation rules for Date
 
+	for idx, item := range m.GetHistoryList() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, GetShareConfReplyValidationError{
+						field:  fmt.Sprintf("HistoryList[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, GetShareConfReplyValidationError{
+						field:  fmt.Sprintf("HistoryList[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return GetShareConfReplyValidationError{
+					field:  fmt.Sprintf("HistoryList[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return GetShareConfReplyMultiError(errors)
 	}
