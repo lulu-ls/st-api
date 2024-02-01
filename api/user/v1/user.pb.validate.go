c26326f352c9d1f043caef41ba199678ee76027f
liuqi <xliuqi@qq.com>
2024-02-01 16:02:10 +0800

    完善: 修改余额不足错误码 495

--- api/user/v1/user.pb.validate.go
+++ api/user/v1/user.pb.validate.go
@@ -1430,6 +1430,10 @@ func (m *SubsidyCheckReply) validate(all bool) error {
 
 	// no validation rules for ItemImage
 
+	// no validation rules for Title
+
+	// no validation rules for Image
+
 	if len(errors) > 0 {
 		return SubsidyCheckReplyMultiError(errors)
 	}
